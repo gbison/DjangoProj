2d5267953971ea1d65bb4807226c1fd16aa02d8d
Bryan Ison <brison@oakthorn.io>
2024-05-20 01:15:35 -0400

    - bootstrap class changes to update the overall cosmetics of the UI - removed the ability to see blog content from cards in show blog as this is now a read blog function. - updated commentary to blogform to help new users understand more proficiently.

--- mydjango/mydjangoprojectweb/blogform.py
+++ mydjango/mydjangoprojectweb/blogform.py
@@ -4,8 +4,8 @@ from mydjangoprojectweb.models import Blog   # Importing the forms Module of Dja
 class BlogForm(forms.ModelForm):    # Define a Modelform which will interact with our Database on backend. Inherits From base class in models.py file under myproject/blogapp directory
       title = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'id':'title','class':'input-field form-control', 'placeholder': "Blog Title", }))
       email = forms.EmailField(widget=forms.TextInput(attrs={'id':'email','class':'input-field form-control', 'placeholder': "Your Email Address"}))
-      content = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'id':'content','class':'textarea form-control', 'rows':'4'}))
+      content = forms.CharField(max_length=1024, widget=forms.Textarea(attrs={'id':'content','class':'textarea form-control', 'rows':'4'}))
 
       class Meta:           # This is mandatory to create form using model form, telling Django the details of how we want it done ie., Which fields from above mentioned blog Model should be used when creating a new entry for this Form – which are all.  
-        model = Blog        # The Database Model that will interact with our database via this form - Here its 'Blog' defined in models.py file under myproject/blogapp directory
+        model = Blog        # The Database Model that will interact with our database via this form - Here its 'Blog' defined in models.py file under mydjango/mydjangoprojectweb directory
         fields = ['title','email','content']    # Fields of the above mentioned blog post creation which need to be filled by users while creating a new entry for Blog Post – here, title & content field are required..  
